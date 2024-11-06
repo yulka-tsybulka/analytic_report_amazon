9a1613daf81691a87158b8e8c4fe01cf4d215e3d
yulka-tsybulka <kasinets.ulia@gmail.com>
2024-11-06 10:47:35 +0200

    first commit

---
+++ src/main/java/analytic/report/amazon/AnalyticReportAmazonApplication.java
@@ -0,0 +1,13 @@
+package analytic.report.amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticReportAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticReportAmazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/analytic/report/amazon/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package analytic.report.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
